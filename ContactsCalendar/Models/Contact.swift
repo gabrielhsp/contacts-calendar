aa4917d332b8709f44375677319832701a487899
Gabriel Henrique <gabhenrique0@gmail.com>
2019-07-23 14:31:40 -0300

    Create a model to parse the json that returns the contact list

---
+++ ContactsCalendar/Models/Contact.swift
@@ -0,0 +1,14 @@
+//
+//  Contact.swift
+//  ContactsCalendar
+//
+//  Created by Gabriel Pereira on 22/07/19.
+//  Copyright © 2019 Gabriel Pereira. All rights reserved.
+//
+
+struct Contact: Codable {
+	let name: String?
+	let phone: String?
+	let company: String?
+	let image: String?
+}
