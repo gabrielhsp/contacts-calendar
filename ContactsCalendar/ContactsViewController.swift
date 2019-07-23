7fcdc615dbeece8a5c5ef95b79244a6cfa09bd45
Gabriel Henrique <gabhenrique0@gmail.com>
2019-07-23 07:44:14 -0300

    Create the base structure of the project

---
+++ ContactsCalendar/ContactsViewController.swift
@@ -0,0 +1,51 @@
+//
+//  ContactsViewController.swift
+//  ContactsCalendar
+//
+//  Created by Gabriel Pereira on 22/07/19.
+//  Copyright © 2019 Gabriel Pereira. All rights reserved.
+//
+
+import UIKit
+
+class ContactsViewController: UIViewController {
+    weak var tableView: UITableView?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        layoutNavigationBar()
+    }
+    
+    func layoutNavigationBar() {
+        self.navigationController?.navigationBar.prefersLargeTitles = true
+        self.navigationController?.navigationBar.barTintColor = .red
+        self.navigationController?.navigationBar.tintColor = .white
+        self.navigationController?.navigationBar.isTranslucent = false
+        self.navigationItem.title = "Contacts"
+    }
+    
+    override func loadView() {
+        super.loadView()
+        
+        let tableViewContacts: UITableView = {
+            let tableView = UITableView()
+            
+            tableView.translatesAutoresizingMaskIntoConstraints = false
+            tableView.backgroundColor = .red
+            tableView.layoutMargins = .zero
+            tableView.separatorInset = .zero
+            
+            return tableView
+        }()
+        
+        self.tableView = tableViewContacts
+        self.view.addSubview(tableViewContacts)
+        
+        NSLayoutConstraint.activate([
+            self.view.safeAreaLayoutGuide.topAnchor.constraint(equalTo: tableViewContacts.topAnchor),
+            self.view.safeAreaLayoutGuide.bottomAnchor.constraint(equalTo: tableViewContacts.bottomAnchor),
+            self.view.safeAreaLayoutGuide.leadingAnchor.constraint(equalTo: tableViewContacts.leadingAnchor),
+            self.view.safeAreaLayoutGuide.trailingAnchor.constraint(equalTo: tableViewContacts.trailingAnchor)
+        ])
+    }
+}
